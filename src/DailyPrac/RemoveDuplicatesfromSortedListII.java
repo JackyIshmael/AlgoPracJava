7c6c3d76754fc90cabe9cf1a07760d620eef822f
王家琦 <382948658@qq.com>
2020-07-01 01:06:50 +0800

    Update

--- src/DailyPrac/RemoveDuplicatesfromSortedListII.java
+++ src/DailyPrac/RemoveDuplicatesfromSortedListII.java
@@ -14,6 +14,41 @@ import java.io.InputStreamReader;
  **/
 public class RemoveDuplicatesfromSortedListII
 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 
-    
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if(head==null||head.next==null)
+                return head;
+
+            ListNode dummy=new ListNode(-1);
+            dummy.next=head;
+            ListNode a=dummy;
+            ListNode b=head.next;
+            while(b!=null){
+
+                if(a.next.val!=b.val){
+                    a=a.next;
+                    b=b.next;
+                }
+                else{
+                    while(b!=null&& a.next.val==b.val){
+                        b=b.next;
+                    }
+                    a.next=b;
+                    b= b==null? null:b.next;
+                }
+
+            }
+            return dummy.next;
+
+        }
+    }
+
 }
